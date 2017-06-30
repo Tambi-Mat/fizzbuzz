86848dfef597908bd86587348b1931710fdc1746
Tambi-Mat <caicaratambi@gmail.com>
2017-06-30 20:36:04 +0100

    test passed for l.c.m of 3 and 5

---
+++ lib/fizzbuzz.rb
@@ -0,0 +1,6 @@
+def fizzbuzz(num)
+  case
+  when num % 3.lcm(15) == 0
+    'fizzbuzz'
+  end
+end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,5 +1,8 @@
+require_relative '../lib/fizzbuzz' # rspec LOAD_PATH for fizzbuzz.rb code file
+
 describe 'fizzbuzz' do
   it 'returns "fizzbuzz" when passed 15' do
-  expect(fizzbuzz(3.lcm(5))).to eq 'fizzbuzz'
+    expect(fizzbuzz(3.lcm(5))).to eq 'fizzbuzz'
   end
+
 end
